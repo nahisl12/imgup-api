30161f9710816e20d23403ce68ff5fa06801b484
Nahidul <nahisl12@gmail.com>
2021-12-30 18:01:33 +0000

    fixed issue with login allowing any passwords to work

--- controllers/login.js
+++ controllers/login.js
@@ -9,10 +9,13 @@ loginRouter.post("/", async (req, res) => {
     const data = req.body;
     // check that the data matches with an user inside mongodb - compare the usernames and passwords
     const user = await User.findOne({ username: data.username }); // Query the database to see if a username matches
-    if (user !== null) {
-      await bcrypt.compare(data.password, user.hash); // compare the password sent with the post request with the hash in database
-    } else {
-      return res.status(401).json({ error: "Invalid username or password" });
+
+    const passwordCorrect = user === null ? false : await bcrypt.compare(data.password, user.hash);
+
+    if (!(user && passwordCorrect)) {
+      return response.status(401).json({
+        error: "invalid username or password",
+      });
     }
 
     const userToSign = {
